7c4ff67c85f41428c3cabcafb48999b67285ccb9
Cmunoz <cmunozm6@miumg.edu.gt>
2025-10-11 10:31:29 -0600

    ejercicio 2 if-else

---
+++ if-else2.py
@@ -0,0 +1,7 @@
+numero = input("Ingresa un número cualquiera de la recta numerica")
+numero = float(numero)
+
+if numero%2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
