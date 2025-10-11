9bcf6e85343bd7b86d2a3e78ccd0efad017ed16c
Cmunoz <cmunozm6@miumg.edu.gt>
2025-10-11 10:26:26 -0600

    ejercicio 1 if-else

---
+++ if-else1.py
@@ -0,0 +1,9 @@
+numero = input("Ingresa un número cualquiera de la recta numerica")
+numero = float(numero)
+
+if numero >= 1:
+    print("El numero es positivo")
+elif numero == 0:
+    print("El numero es cero")
+else:
+    print("El numero es negativo")
