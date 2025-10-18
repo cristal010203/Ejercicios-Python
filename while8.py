a891b95eb922bdd7e3e632b19512495e9e85ba15
Cmunoz <cmunozm6@miumg.edu.gt>
2025-10-18 08:59:57 -0600

    ejercicio 8 while

---
+++ while8.py
@@ -0,0 +1,5 @@
+num = int(input("Ingresa un número (0 para salir): "))
+while num != 0:
+    print("Ingresaste:", num)
+    num = int(input("Ingresa otro número (0 para salir): "))
+print("Programa finalizado.")
